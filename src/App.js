c765bff88248d48eef6f2996ced2d51f033c568d
Ahmed Rezk <ahmedrezk@Ahmeds-MacBook-Pro.local>
2020-09-11 00:22:57 -0600

    Volume | Muted | Unmuted Status

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { FaPlay, FaPause } from 'react-icons/fa';
+import { FaPlay, FaPause, FaVolumeMute, FaVolumeUp } from 'react-icons/fa';
 
 function App() {
 	const videoEl = useRef();
 
 	const [play, setPlay] = useState(false);
+	const [muted, setMuted] = useState(false);
+	const [volume, setVolume] = useState(0);
 
 	// handle Play video event
 	const handleOnPlay = () => {
@@ -17,6 +19,21 @@ function App() {
 	const handleOnPause = () => {
 		setPlay(false);
 	};
+
+	// handle Volume Change event
+	const handleVolumeChange = () => {
+		console.log(videoEl.current.volume);
+		setMuted(videoEl.current.muted);
+		if (videoEl.current.volume > 0) {
+			setVolume(videoEl.current.volume);
+		} else {
+			setMuted(true);
+		}
+	};
+
+	useEffect(() => {
+		setVolume(videoEl.current.volume);
+	}, []);
 
 	return (
 		<div className="App">
@@ -42,6 +59,7 @@ function App() {
 								preload="none"
 								onPlay={handleOnPlay}
 								onPause={handleOnPause}
+								onVolumeChange={handleVolumeChange}
 							></video>
 						</div>
 					</div>
@@ -63,6 +81,22 @@ function App() {
 								)}
 							</div>
 							{/* END: playing status */}
+
+							{/* START: Volume status */}
+							<div className="col-md-3  status-container">
+								{muted ? (
+									<>
+										<FaVolumeMute color="#4ad9db" size="30" />
+										<span>Muted</span>
+									</>
+								) : (
+									<>
+										<FaVolumeUp color="#4ad9db" size="30" />
+										<span>{parseInt(volume * 10)}</span>
+									</>
+								)}
+							</div>
+							{/* END: Volume status */}
 						</div>
 					</div>
 				</div>
