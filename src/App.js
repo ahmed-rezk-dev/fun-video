5c3effc2f24047ce72cf39b00aa8223543d63acc
Ahmed Rezk <ahmedrezk@Ahmeds-MacBook-Pro.local>
2020-09-11 00:17:26 -0600

    Video player

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const videoEl = useRef();
+	return (
+		<div className="App">
+			<div className="container">
+				{/* START: header */}
+				<header className="header">
+					<p className="title" data-heading="Fun Videos">
+						Fun Videos
+					</p>
+				</header>
+				{/* END: header */}
+
+				<div className="row">
+					{/* START: video player */}
+					<div className="col-md-12">
+						<div className="video-container text-center">
+							<video
+								ref={videoEl}
+								controls
+								className="video-player"
+								src="https://s3.eu-central-1.amazonaws.com/pipe.public.content/short.mp4"
+								poster="https://s3.eu-central-1.amazonaws.com/pipe.public.content/poster.png"
+								preload="none"
+							></video>
+						</div>
+					</div>
+					{/* END: video player */}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
